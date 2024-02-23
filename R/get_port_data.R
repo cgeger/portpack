6d14f90676d0acef54b0d60a0d0b506f7df00cc2
Caitlin Eger <cgeger@gmail.com>
2024-02-23 16:39:53 -0500

    correct funnction export

--- R/get_port_data.R
+++ R/get_port_data.R
@@ -2,10 +2,9 @@
 #'
 #' @return A data frame with the following columns: port_number, port_border, port_name, port_crossing_name, port_hours, port_status, port_commercial_automation_type, port_commercial_max_lanes, port_passenger_automation_type, port_passenger_max_lanes, port_pedestrian_automation_type, port_pedestrian_max_lanes
 #' @import dplyr
-#' @export get_port_data
-#'
+#' @export
 get_port_data <- function(){
   scrape_df <- scrape_ports()
-  port_data <- scrape_df %>% select(starts_with("port"), -port_status)
+  port_data <- scrape_df %>% select(starts_with("port"), -"port_status")
   return(port_data)
 }
