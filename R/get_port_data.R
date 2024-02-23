3ea0671a618493997edaf86e2a7675bfa636cb94
Caitlin Eger <cgeger@gmail.com>
2024-02-23 17:14:10 -0500

    removes port_status from list of static port information

--- R/get_port_data.R
+++ R/get_port_data.R
@@ -1,6 +1,7 @@
 #' Lists all the ports and their basic information
 #'
-#' @return A data frame with the following columns: port_number, port_border, port_name, port_crossing_name, port_hours, port_status, port_commercial_automation_type, port_commercial_max_lanes, port_passenger_automation_type, port_passenger_max_lanes, port_pedestrian_automation_type, port_pedestrian_max_lanes
+#' @return A data frame with the following columns: port_number, port_border, port_name, port_crossing_name, port_hours, port_commercial_automation_type, port_commercial_max_lanes, port_passenger_automation_type, port_passenger_max_lanes, port_pedestrian_automation_type, port_pedestrian_max_lanes
+#'
 #' @import dplyr
 #' @export
 get_port_data <- function(){
