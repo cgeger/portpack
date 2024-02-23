351775454600ac74511b267553fa06db1016588e
Caitlin Eger <cgeger@gmail.com>
2024-02-23 16:45:34 -0500

    correct warning message with .data from rlang

--- R/get_port_status.R
+++ R/get_port_status.R
@@ -1,18 +1,22 @@
 #' gets the current status for each port
 #'
 #' @return a dataframe with the current status for each port
-#'
+#' @import dplyr
+#' @import tidyr
+#' @import tidyselect
+#' @importFrom rlang .data
+#' @export
 get_port_status <- function(){
-  scrape <- get_port_data()
-  port_status <- scrape %>%
-    select(port_number, starts_with("commercial"),
+  scrape <- scrape_ports()
+  (port_status <- scrape %>%
+    select(.data$port_number, starts_with("commercial"),
            starts_with("passenger"),
            starts_with("pedestrian")) %>%
     pivot_longer(cols = c(starts_with("commercial"),
                           starts_with("passenger"),
                           starts_with("pedestrian")), names_to = "lane_type") %>%
-    filter(!value %in% c("", "N/A")) %>%
-    separate(lane_type, into = c("modality", "lane_type", "observation"), sep = "\\.") %>%
-    pivot_wider(names_from = "observation", values_from = "value") %>% as.data.frame()
+    filter(!.data$value %in% c("", "N/A")) %>%
+    separate(.data$lane_type, into = c("modality", "lane_type", "observation"), sep = "\\.") %>%
+    pivot_wider(names_from = "observation", values_from = "value") %>% as.data.frame())
   return(port_status)
 }
