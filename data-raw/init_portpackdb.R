5dafa83d147a6033216e51d4c4c6c748fd90e64d
Caitlin Eger <cgeger@gmail.com>
2024-02-23 16:46:39 -0500

    create external database for port status collection

--- data-raw/init_portpackdb.R
+++ data-raw/init_portpackdb.R
@@ -1,7 +1,7 @@
 #' initialize port data database
 #'
-portpackdb <- function(name = "portpack.db") {
-  con <- DBI::dbConnect(RSQLite::SQLite(), paste0("data/", name))
+portpackdb <- function(db_name = "portpack.db") {
+  con <- DBI::dbConnect(RSQLite::SQLite(), paste0("inst/extdata/", db_name))
 
   port_data <- get_port_data()
   DBI::dbWriteTable(con, "port_data", port_data, overwrite = TRUE)
